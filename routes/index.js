3be23ee372b273ccad20b208676875c927a3a684
jimcute <jimcute8879@gmail.com>
2017-01-25 23:21:48 +0530

    make it better

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var icloud = require("find-my-iphone").findmyphone;
+icloud.apple_id = "jameer.1993@icloud.com";
+icloud.password = "Appleipad2";
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/api/get-my-phone-location', function (req, res, next) {
+    icloud.getDevices(function (error, devices) {
+        var device;
+
+        if (error) {
+            res.status(401).send(error);
+        }
+        //pick a device with location and findMyPhone enabled 
+        devices.forEach(function (d) {
+            if (device == undefined && d.location && d.lostModeCapable) {
+                device = d;
+            }
+        });
+
+        if (device) {
+
+            icloud.getLocationOfDevice(device, function (err, location) {
+                console.log(location);
+                res.send(location);
+            });
+        } else {
+            res.status(401).send('Device not found');
+        }
+    });
+});
+
+module.exports = router;
